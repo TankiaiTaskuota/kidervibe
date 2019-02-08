94b2a136b5156df31a9c717f505b7f721c274eca
Vitalija Korkuc <tankiaitaskuota@gmail.com>
2019-02-08 21:03:04 +0200

    version up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   resources :images
   resources :food_activities
   resources :foods
   resources :app_days
   resources :childerns
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
